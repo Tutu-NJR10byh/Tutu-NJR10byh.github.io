2f1501bcf43a33397a488b7e57d0f34b44cc4283
Tutu-NJR10byh <76162631+Tutu-NJR10byh@users.noreply.github.com>
2020-12-18 11:14:41 +0800

    Update Exam.js

--- js/Exam.js
+++ js/Exam.js
@@ -6,36 +6,36 @@ function Tologin() {
 }
 function Show() {
   document.getElementById("Outside").style.opacity = "1";
-}
-function two_char(n) {
-  return n >= 10 ? n : "0" + n;
-}
-function time_fun() {
   console.log("aaa");
   var m = 1;
   var s = 10;
-  setInterval(function () {
+  var timer = setInterval(function () {
     if (m >= 0) {
       if (s < 10) {
-        $(".time").html("剩余时间：" + m + ":0" + s);
+        $("#time").html("剩余时间: " + "&nbsp" + m + ":0" + s);
       } else {
-        $(".time").html("剩余时间：" + m + ":" + s);
+        $("#time").html("剩余时间: " + "&nbsp" + m + ":" + s);
+      }
+      if (m == 1 && s == 0) {
+        alert("答题时间仅剩 1 分钟，时间到题目将自动提交！！");
+      }
+      if (m == 0 && s <= 0) {
+        alert("时间到，题目已自动提交！");
+        clearInterval(timer);
       }
       s--;
       if (s < 0) {
         s = 59;
         m--;
       }
-      if (m == 0 && s < 1) {
-        window.location.href =
-          "result.html?radio=" + 5 + "checkbox=" + 2 + "&json=" + getjson();
-      }
     }
   }, 1000);
 }
+function two_char(n) {
+  return n >= 10 ? n : "0" + n;
+}
 
-function main() {
-  console.log("aa");
+function submit() {
   var questionArray = new Array("Q1", "Q2", "Q3", "Q4", "Q5");
   var resultArray = new Array();
   var rightArray = new Array();
@@ -46,37 +46,35 @@ function main() {
     if (Name(questionArray[i]) != 10) {
       resultArray[i] = Name(questionArray[i]);
     } else {
-      alert("第" + (i + 1) + "题,您未作答!!");
+      alert("第 " + (i + 1) + " 题 " + " 您未作答 !!");
       return false;
     }
   }
-  var right_number = 0; //计算答对的题数；
-  for (var i = 0; i < questionArray.length; i++) {
-    if (aryAns[i] == resultArray[i]) {
-      right_number++;
-      rightArray[i] = 1;
-    } else {
-      rightArray[i] = 0;
-    }
-  }
-  var right_question = " ";
-  var error_question = " ";
-  for (var i = 0; i < rightArray.length; i++) {
-    if (rightArray[i] == 1) {
-      right_question += i + 1 + ",";
-    } else {
-      error_question += i + 1 + ",";
-    }
-  }
-  var time = document.getElementById("mytime").innerHTML;
-  document.getElementById("right_number").innerText = right_number;
-  document.getElementById("time").innerText = time;
-  if (right_question != " ") {
-    document.getElementById("right_question").innerText = right_question;
-  }
-  if (error_question != " ") {
-    document.getElementById("error_question").innerText = error_question;
-  }
+  // var right_number = 0; //计算答对的题数；
+  // for (var i = 0; i < questionArray.length; i++) {
+  //   if (aryAns[i] == resultArray[i]) {
+  //     right_number++;
+  //     rightArray[i] = 1;
+  //   } else {
+  //     rightArray[i] = 0;
+  //   }
+  // }
+  // var right_question = " ";
+  // var error_question = " ";
+  // for (var i = 0; i < rightArray.length; i++) {
+  //   if (rightArray[i] == 1) {
+  //     right_question += i + 1 + ",";
+  //   } else {
+  //     error_question += i + 1 + ",";
+  //   }
+  // }
+  // document.getElementById("right_number").innerText = right_number;
+  // if (right_question != " ") {
+  //   document.getElementById("right_question").innerText = right_question;
+  // }
+  // if (error_question != " ") {
+  //   document.getElementById("error_question").innerText = error_question;
+  // }
 }
 
 function Name(name) {
